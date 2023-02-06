9690c0c8561c8621a588b227edded308577371a7
Ricardo Duarte <rickduarte@gmail.com>
2023-02-06 11:10:42 -0300

    connect to web service

--- lib/models/location.g.dart
+++ lib/models/location.g.dart
@@ -7,6 +7,7 @@ part of 'location.dart';
 // **************************************************************************
 
 Location _$LocationFromJson(Map<String, dynamic> json) => Location(
+      id: json['id'] as int,
       name: json['name'] as String,
       url: json['url'] as String,
       facts: (json['facts'] as List<dynamic>)
@@ -15,6 +16,7 @@ Location _$LocationFromJson(Map<String, dynamic> json) => Location(
     );
 
 Map<String, dynamic> _$LocationToJson(Location instance) => <String, dynamic>{
+      'id': instance.id,
       'name': instance.name,
       'url': instance.url,
       'facts': instance.facts,
